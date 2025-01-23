384e9ccef3df0e732ae7c710d2cd949c684a8536
tomegoodluck4 <tomegoodluck4@gmail.com>
2025-01-23 20:05:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "Gracefullbody@gmail.com";
-const pass = "gdqjcbleqkugtoni";
+//
+//const pass = "gdqjcbleqkugtoni";
 // young exchange 23 feb
 
 // Middleware
